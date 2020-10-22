0e9780cd984f09c5ecd75adbc36d0f6263799b47
99002677 <71485168+99002677@users.noreply.github.com>
2020-10-22 09:11:26 +0530

    Create client.h
    client header files

---
+++ include/client.h
@@ -0,0 +1,12 @@
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
+#include <signal.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+
+#endif
