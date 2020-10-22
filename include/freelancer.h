0e9fa25afde3a957abd0d960d42244ec7e879261
99002676 <71481415+99002676@users.noreply.github.com>
2020-10-22 09:13:50 +0530

    Add files via upload
    Freelancer header file done

---
+++ include/freelancer.h
@@ -0,0 +1,14 @@
+#ifndef __FREELANCER_H__
+#define __FREELANCER_H__
+
+#include <signal.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+
+#endif
